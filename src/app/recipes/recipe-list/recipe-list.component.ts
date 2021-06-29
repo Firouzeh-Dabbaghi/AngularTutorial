38edd3ddd446b64920d22be84daad1ee15998180
Firouzeh-Dabbaghi <70266008+Firouzeh-Dabbaghi@users.noreply.github.com>
2021-06-29 17:52:30 +0430

    outputing a list of recepie with ng for

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -7,6 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes: Recipe[] = [
+    new Recipe(
+      'Mediterranean Breakfast Burrata Platter',
+      'There s never a wrong time to eat burrata. This healthy but hearty recipe will quickly become one of your go-to dishes, and its perfect to serve if youre hosting brunch.',
+      'https://i2.wp.com/www.eatthis.com/wp-content/uploads/2019/11/breakfast-burrata.jpg?resize=640%2C468&ssl=1')
+  ];
   constructor() { }
 
   ngOnInit(): void {
