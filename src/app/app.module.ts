e0be5d0ac8d217b464e030d2d1e844b2026abfc7
Firouzeh-Dabbaghi <70266008+Firouzeh-Dabbaghi@users.noreply.github.com>
2021-06-29 12:58:30 +0430

    reset aplication for new practice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { SuccesAlertComponent } from './succes-alert/succes-alert.component';
-import { WarningAlertComponent } from './warnin-alert/warning-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SuccesAlertComponent,
-    WarningAlertComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
